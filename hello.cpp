1cd4f4d6dd560e240fbaee1d08c3ab8ac6da0932
Yu-Muro <6u38e7c16rnhg5e37a0z@gmail.com>
2023-02-11 20:25:18 +0900

    Feat: vectorの標準入出力マクロを追加

--- hello.cpp
+++ hello.cpp
@@ -1,7 +1,10 @@
 #include <bits/stdc++.h>
+
 using namespace std;
+
 typedef long long ll;
 typedef long double ld;
+
 #define mp make_pair
 #define pb(a) push_back(a)
 #define all(a) (a).begin(), (a).end()
@@ -15,7 +18,10 @@ typedef long double ld;
 #define REPD(i, n) for (ll i = n; i > 0; --i)
 #define REP2(i, a, b) for (ll i = a; i < b; ++i)
 #define REPD2(i, a, b) for (ll i = a; i > b; --i)
-#define printd(a, n) cout << std::fixed << std::setprecision(n) << a << endl
+#define printd(a, n) cout << fixed << setprecision(n) << a << endl
+#define inputvec(v) REP(i, v.size()) cin >> v[i]
+#define printvec(v) cout << v[0]; REP(i, v.size()-1) cout << ' ' << v[i+1]; cout << endl;
+
 
 int main() {
     cout << "Hello" << endl;
