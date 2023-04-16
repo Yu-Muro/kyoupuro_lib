076e251545d5d255398fb4c26cc21fc474019b7a
Yu-Muro <6u38e7c16rnhg5e37a0z@gmail.com>
2023-04-16 14:01:49 +0900

    Add: ABC298

---
+++ C++/ABC298/a.cpp
@@ -0,0 +1,53 @@
+#include <bits/stdc++.h>
+#include <atcoder/all>
+using namespace std;
+using namespace atcoder;
+typedef long long ll;
+typedef long double ld;
+typedef unsigned long long ull;
+#define mp make_pair
+#define pb(a) push_back(a)
+#define all(a) (a).begin(), (a).end()
+#define rall(a) (a).rbegin(), (a).rend()
+#define pll pair<ll, ll>
+#define vecll vector<ll>
+#define vecll2 vector<vecll>
+#define vecld vector<ld>
+#define vecld2 vector<vecld>
+#define REP(i, n) for (ll i = 0; i < n; ++i)
+#define REPD(i, n) for (ll i = n; i > 0; --i)
+#define REP2(i, a, b) for (ll i = a; i < b; ++i)
+#define REPD2(i, a, b) for (ll i = a; i > b; --i)
+#define printd(a, n) cout << fixed << setprecision(n) << a << endl
+#define inputvec(v) \
+    for (ull i = 0; i < v.size(); ++i) cin >> v[i]
+#define pbllvec(v, n)             \
+    for (ull i = 0; i < n; i++) { \
+        ll temp;                  \
+        cin >> temp;              \
+        v.pb(temp);               \
+    }
+#define printvec(v)                                                 \
+    cout << v[0];                                                   \
+    for (ull i = 0; i < v.size() - 1; ++i) cout << ' ' << v[i + 1]; \
+    cout << endl;
+
+int main() {
+    dsu d;
+    int n;
+    cin >> n;
+    string s;
+    cin >> s;
+    string ans = "No";
+    bool yes   = false;
+    for (const auto& w : s) {
+        if (w == 'o') yes = true;
+        if (w == 'x') {
+            cout << ans << endl;
+            return 0;
+        }
+    }
+    if (yes) ans = "Yes";
+    cout << ans << endl;
+    return 0;
+}
