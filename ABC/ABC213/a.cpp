2f8b4823e41bb2c926e977d0d21b57aee8066f96
Yu MUROTANI <y_murotani@yumemi.co.jp>
2023-11-25 16:18:48 +0900

    add: ABC213-a-ac

---
+++ ABC/ABC213/a.cpp
@@ -0,0 +1,33 @@
+#include <bits/stdc++.h>
+#include <atcoder/all>
+using namespace std;
+using namespace atcoder;
+typedef long long ll;
+typedef long double ld;
+#define mp make_pair
+#define pb(a) push_back(a)
+#define all(a) (a).begin(), (a).end()
+#define rall(a) (a).rbegin(), (a).rend()
+#define pint pair<int, int>
+#define plong pair<ll, ll>
+#define vecint vector<int>
+#define vecll vector<ll>
+#define vecll2 vector<vecll>
+#define vecld vector<ld>
+#define vecld2 vector<vecld>
+#define REP(i, n) for (ll i = 0; i < n; ++i)
+#define REPD(i, n) for (ll i = n; i > 0; --i)
+#define REP2(i, a, b) for (ll i = a; i < b; ++i)
+#define REPD2(i, a, b) for (ll i = a; i > b; --i)
+#define printd(a, n) cout << std::fixed << std::setprecision(n) << a << endl
+
+int main() {
+    int a, b; cin >> a >> b;
+    REP(i, 256) {
+        if((a ^ i) == b) {
+            cout << i << endl;
+            return 0;
+        }
+    }
+    return 0;
+}
