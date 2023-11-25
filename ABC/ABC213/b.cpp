abfbfc5d208fbe7c302ba732ffea8cda013c9b7d
Yu MUROTANI <y_murotani@yumemi.co.jp>
2023-11-25 16:19:03 +0900

    add: ABC213-b-ac

---
+++ ABC/ABC213/b.cpp
@@ -0,0 +1,34 @@
+#include <bits/stdc++.h>
+#include <atcoder/all>
+using namespace std;
+using namespace atcoder;
+typedef long long ll;
+typedef long double ld;
+#define mp make_pair
+#define pb(a) push_back(a)
+#define all(a) (a).begin(), (a).end()
+#define rall(a) (a).rbegin(), (a).rend()
+#define pint pair<int, int>
+#define plong pair<ll, ll>
+#define vecint vector<int>
+#define vecll vector<ll>
+#define vecll2 vector<vecll>
+#define vecld vector<ld>
+#define vecld2 vector<vecld>
+#define REP(i, n) for (ll i = 0; i < n; ++i)
+#define REPD(i, n) for (ll i = n; i > 0; --i)
+#define REP2(i, a, b) for (ll i = a; i < b; ++i)
+#define REPD2(i, a, b) for (ll i = a; i > b; --i)
+#define printd(a, n) cout << std::fixed << std::setprecision(n) << a << endl
+
+int main() {
+    int n; cin >> n;
+    vector<plong> a(n);
+    REP(i, n) {
+        ll tmp; cin >> tmp;
+        a[i] = mp(tmp, i+1);
+    }
+    sort(all(a));
+    cout << a[n-2].second << endl;
+    return 0;
+}
