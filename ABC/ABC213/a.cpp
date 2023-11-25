81519db9f590c9fb98c9c9a9fe49a08fdf662e81
Yu MUROTANI <6u38e7c16rnhg5e37a0z@gmail.com>
2023-11-25 16:24:49 +0900

    ABを埋める (#3)

--- ABC/ABC213/a.cpp
+++ ABC/ABC213/a.cpp
@@ -1,12 +1,9 @@
 #include <bits/stdc++.h>
 #include <atcoder/all>
-
 using namespace std;
 using namespace atcoder;
-
 typedef long long ll;
 typedef long double ld;
-
 #define mp make_pair
 #define pb(a) push_back(a)
 #define all(a) (a).begin(), (a).end()
@@ -22,9 +19,15 @@ typedef long double ld;
 #define REPD(i, n) for (ll i = n; i > 0; --i)
 #define REP2(i, a, b) for (ll i = a; i < b; ++i)
 #define REPD2(i, a, b) for (ll i = a; i > b; --i)
-#define printd(a, n) cout << fixed << setprecision(n) << a << endl
+#define printd(a, n) cout << std::fixed << std::setprecision(n) << a << endl
 
 int main() {
-    cout << "Hello" << endl;
+    int a, b; cin >> a >> b;
+    REP(i, 256) {
+        if((a ^ i) == b) {
+            cout << i << endl;
+            return 0;
+        }
+    }
     return 0;
 }
