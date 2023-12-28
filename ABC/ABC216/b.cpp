5db6fd1274642c67129a55e6722cb9594c6f363e
Yu MUROTANI <y_murotani@yumemi.co.jp>
2023-12-28 17:06:51 +0900

    ABC216

---
+++ ABC/ABC216/b.cpp
@@ -0,0 +1,39 @@
+#include <bits/stdc++.h>
+
+#include <atcoder/all>
+
+using namespace std;
+using namespace atcoder;
+
+typedef long long ll;
+typedef long double ld;
+
+#define mp make_pair
+#define pb(a) push_back(a)
+#define all(a) (a).begin(), (a).end()
+#define rall(a) (a).rbegin(), (a).rend()
+#define pint pair<int, int>
+#define plong pair<ll, ll>
+#define vecint vector<int>
+#define vecll vector<ll>
+#define vecll2 vector<vecll>
+#define vecld vector<ld>
+#define vecld2 vector<vecld>
+#define len(a) (ll)(a).size()
+#define REP(i, n) for (ll i = 0; i < n; ++i)
+#define REPD(i, n) for (ll i = n; i > 0; --i)
+#define REP2(i, a, b) for (ll i = a; i < b; ++i)
+#define REPD2(i, a, b) for (ll i = a; i > b; --i)
+#define printd(a, n) cout << fixed << setprecision(n) << a << endl
+
+int main() {
+    int n; cin >> n;
+    set<string> name;
+    REP(i, n) {
+        string s, t; cin >> s >> t;
+        name.insert(s + ' ' + t);
+    }
+    if(len(name) != n) cout << "Yes" << endl;
+    else cout << "No" << endl;
+    return 0;
+}
